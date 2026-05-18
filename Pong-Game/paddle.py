4098c52b46dd1947b45009990fe0dc80babe9d70
Aditisharma1205 <aditish663@gmail.com>
2026-05-19 00:26:04 +0530

    Update paddle.py

--- Pong-Game/paddle.py
+++ Pong-Game/paddle.py
@@ -7,9 +7,11 @@ class Paddle(Turtle):
             self.shapesize(5,1)
             self.penup()
             self.goto(position)
+        
     def go_up(self):
         new_y= self.ycor()+30
         self.goto(self.xcor(),new_y)
+        
     def go_down(self):
         new_y = self.ycor()-30
         self.goto(self.xcor(), new_y)
