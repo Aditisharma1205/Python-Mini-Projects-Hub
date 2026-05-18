556c79ffd4ce5f4b66dd77404e15a3d34f7eef89
Aditisharma1205 <aditish663@gmail.com>
2026-05-19 00:26:35 +0530

    Update scorecard.py

--- Pong-Game/scorecard.py
+++ Pong-Game/scorecard.py
@@ -9,15 +9,18 @@ class Scorecard(Turtle):
         self.l_score=0
         self.r_score=0
         self.update_score()
+        
     def update_score(self):
         self.clear()
         self.goto(-100,200)
         self.write(self.l_score,align="center",font=FONT)
         self.goto(100,200)
         self.write(self.r_score,align="center",font=FONT)
+        
     def l_point(self):
         self.l_score+=1
         self.update_score()
+        
     def r_point(self):
         self.r_score+=1
         self.update_score()
