a01b53eff1bac1bce28ad934149b91dba5b7956d
Aditisharma1205 <aditish663@gmail.com>
2026-05-20 00:45:14 +0530

    Add main game loop for Turtle Crossing

---
+++ Turtle-Crossing-Capstone/main.py
@@ -0,0 +1,34 @@
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+import time
+screen = Screen()
+screen.tracer(0)
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+screen.setup(width=600, height=600)
+screen.bgcolor("white")
+screen.title("Turtle Crossing Capstone")
+screen.listen()
+screen.onkeypress(player.up, "Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    car_manager.create_cars()
+    car_manager.move_cars()
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    if player.is_at_finish_line():
+        player.go_to_starting_position()
+        car_manager.level_up()
+        scoreboard.increase_level()
+
+
+screen.exitonclick()
