f1a0b2fa4397b49a96d65bde92dc42e14e04ad7a
Aditisharma1205 <aditish663@gmail.com>
2026-05-24 22:44:29 +0530

    Refactor guessing logic and improve state handling

--- US States Guessing Game/main.py
+++ US States Guessing Game/main.py
@@ -8,17 +8,17 @@ turtle.shape(image)
 data=pd.read_csv("50_states.csv")
 all_states= data.state.to_list()
 guessed_states=[]
+
 while len(guessed_states)<50:
     answer_state=screen.textinput(f"{len(guessed_states)}/50 States Correct","What's another state's name?").title()
+    
     if answer_state == "Exit":
-        missing_states=[]
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states=[state for state in all_states if state not in guessed_states]
         new_data=pd.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
-    if answer_state in all_states:
+    
+    if answer_state in all_states and answer_state not in guessed_states:
         guessed_states.append(answer_state)
         t=turtle.Turtle()
         t.hideturtle()
@@ -26,5 +26,3 @@ while len(guessed_states)<50:
         state_data=data[data.state==answer_state]
         t.goto(state_data.x.item(),state_data.y.item())
         t.write(answer_state)
-
-
