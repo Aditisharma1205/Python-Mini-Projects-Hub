3d8ce8fe67ae918f5574548a2f343cbb73903db2
Aditisharma1205 <aditish663@gmail.com>
2026-05-28 23:13:50 +0530

    Update main.py

--- Pong-Game/main.py
+++ Pong-Game/main.py
@@ -3,20 +3,24 @@ from paddle import Paddle
 from ball import Ball
 from scorecard import Scorecard
 import time
+
 screen = Screen()
 screen.setup(800,600)
 screen.bgcolor("black")
 screen.title("Pong")
 screen.tracer(0)
+
 r_paddle=Paddle((380,0))
 l_paddle=Paddle((-380,0))
 ball=Ball()
 scoreboard=Scorecard()
+
 screen.listen()
 screen.onkey(r_paddle.go_up,"Up")
 screen.onkey(r_paddle.go_down,"Down")
 screen.onkey(l_paddle.go_up,"w")
 screen.onkey(l_paddle.go_down,"s")
+
 game_is_on=True
 while game_is_on:
     time.sleep(ball.moving_speed)
